b98598c6d0d3c82330abe867c7a2ae3141ae3eef
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2021-03-28 20:18:05 -0400

    direct GitHub autoupdate
    todo: implement downloads directly rather than call a browser

--- src/main-update.js
+++ src/main-update.js
@@ -1,58 +1,27 @@
-const { BrowserWindow, ipcMain, autoUpdater } = require('electron');
-// const messenger = require(path.join(__dirname,'main-messaging.js'));
+const { BrowserWindow, ipcMain, app } = require('electron');
+const fetch = require('node-fetch');
+const semver = require('semver');
 const log = require('electron-log');
-
-autoUpdater.channel = 'latest';
-autoUpdater.allowDowngrade = false;
-
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-autoUpdater.logger.transports.file.appName = 'squirrel-table';
-autoUpdater.autoDownload = false;
-// autoUpdater.setFeedURL('http://127.0.0.1:8080');
 
 const updateError = (error) => {
   BrowserWindow.fromId(global.mainWindowId).webContents.send('update-error', error);
 };
-// The following is triggered on top of updateError() already called in .catch
-autoUpdater.on('error', (error) => {
-  // updateError(error);
-});
-/*
-autoUpdater.on('checking-for-update', () => {
-});
-*/
-autoUpdater.on('update-available', (info) => {
-  // info = json obj of latest.yml
-  BrowserWindow.fromId(global.mainWindowId).webContents.send('update-available', info);
-});
-autoUpdater.on('update-not-available', (info) => {
-  // info = json obj of latest.yml
-  BrowserWindow.fromId(global.mainWindowId).webContents.send('update-not-available');
-});
 
 ipcMain.on('check-for-updates',(event) => {
-  autoUpdater.checkForUpdates().catch(err => { updateError(err) });
+  fetch('https://api.github.com/repos/reiniiriarios/squirrel-table/releases?per_page=1').then(res => res.json()).then(releases => {
+    console.log(releases);
+    let latestVersion = semver.clean(releases[0].tag_name);
+    if (semver.lte(latestVersion, app.getVersion())) {
+      //todo: filter by process.platform
+      //todo: use request to implement direct downloads
+      let downloadUrl = 'https://github.com/reiniiriarios/squirrel-table/releases/download/' + releases[0].tag_name + '/SquirrelTableSetup.exe';
+      BrowserWindow.fromId(global.mainWindowId).webContents.send('update-available', downloadUrl);
+    }
+    else {
+      BrowserWindow.fromId(global.mainWindowId).webContents.send('update-not-available');
+    }
+  }).catch(err => {
+    log.error(err);
+    updateError(err);
+  });
 });
-
-/*
-STOP: Cannot continue with autoupdater without digitally signing updates
-
-ipcMain.on('download-update',(event) => {
-  autoUpdater.downloadUpdate().catch(err => { updateError(err) });
-})
-autoUpdater.on('download-progress', (progressObj) => {
-  // progressObj.bytesPerSecond
-  //            .percent
-  //            .transferred
-  //            .total
-  BrowserWindow.fromId(global.mainWindowId).webContents.send('download-progress', progressObj);
-});
-autoUpdater.on('update-downloaded', (info) => {
-  // info = json obj of latest.yml
-  BrowserWindow.fromId(global.mainWindowId).webContents.send('update-downloaded');
-});
-ipcMain.on('update-app',(event) => {
-  autoUpdater.quitAndInstall().catch(err => { updateError(err) });
-});
-*/
--- src/renderer-update.js
+++ src/renderer-update.js
@@ -7,69 +7,34 @@ let updateDownloadLink;
 
 updateButton.on('click',(event) => {
   if (updateButton.attr('disabled')) return false;
-
   switch (updateProgress) {
     case 'update-available':
       event.preventDefault();
       shell.openExternal(updateDownloadLink);
       updateButton.attr('disabled',true).addClass('disabled').text('Downloading');
+      setTimeout(() => {
+        updateProgress = false;
+        updateButton.attr('disabled',false).removeClass('disabled').text('Check for Updates');
+        updateStatusMsg.text(' ');
+      }, 5000);
       break;
-    /*
-    STOP: Cannot continue with autoupdater without digitally signing updates
-
-    case 'update-available':
-      updateButton.attr('disabled',true).addClass('disabled').text('Downloading');
-      ipcRenderer.send('download-update');
-      break;
-    case 'update-downloaded':
-      updateButton.attr('disabled',true).addClass('disabled').text('Installing');
-      ipcRenderer.send('update-app');
-      break;
-    */
     default:
       updateButton.attr('disabled',true).addClass('disabled').text('Checking');
       ipcRenderer.send('check-for-updates');
   }
 });
 
-ipcRenderer.on('update-available',(event, info) => {
-  info.files.forEach(file => {
-    // separate by platform here
-    if (file.url.substr(file.url.length - 4) == '.exe') {
-      updateDownloadLink = file.url;
-      updateProgress = 'update-available';
-      updateButton.attr('disabled',false).removeClass('disabled').text('Download Update');
-      updateStatusMsg.text('Update Available');
-    }
-  });
-  setTimeout(() => {
-    if (updateDownloadLink.length == 0) {
-      updateProgress = false;
-      updateButton.attr('disabled',false).removeClass('disabled').text('Check for Updates');
-      updateStatusMsg.html('<span class="error">Error updating, see log for details</span>');
-      console.log('No appropriate download found.');
-      console.log(info);
-    }
-  }, 500);
+ipcRenderer.on('update-available',(event, downloadUrl) => {
+  updateDownloadLink = downloadUrl;
+  updateProgress = 'update-available';
+  updateButton.attr('disabled',false).removeClass('disabled').text('Download Update');
+  updateStatusMsg.text('Update Available');
 });
 ipcRenderer.on('update-not-available',(event) => {
   updateProgress = false;
   updateButton.attr('disabled',false).removeClass('disabled').text('Check for Updates');
   updateStatusMsg.text('No Updates Available');
 });
-
-/*
-STOP: Cannot continue with autoupdater without digitally signing updates
-
-ipcRenderer.on('download-progress',(event, progressObj) => {
-  updateStatusMsg.text(progressObj.percent + '% (' + progressObj.transferred + '/' + progressObj.total + ')');
-})
-ipcRenderer.on('update-downloaded',(event) => {
-  updateProgress = 'update-downloaded';
-  updateButton.attr('disabled',false).removeClass('disabled').text('Update & Quit');
-  updateStatusMsg.text('Update downloaded');
-});
-*/
 
 ipcRenderer.on('update-error',(event, error) => {
   updateProgress = false;
