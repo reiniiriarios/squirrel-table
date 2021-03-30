5f084ac4ce18d8cb01baef3be325ff88530b2b39
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2021-03-30 13:01:28 -0400

    update bug

--- src/main-update.js
+++ src/main-update.js
@@ -10,7 +10,7 @@ const updateError = (error) => {
 ipcMain.on('check-for-updates',(event) => {
   fetch('https://api.github.com/repos/reiniiriarios/squirrel-table/releases?per_page=1').then(res => res.json()).then(releases => {
     let latestVersion = semver.clean(releases[0].tag_name);
-    if (semver.gte(latestVersion, app.getVersion())) {
+    if (semver.gt(latestVersion, app.getVersion())) {
       //todo: filter by process.platform
       //todo: use request to implement direct downloads
       let downloadUrl = 'https://github.com/reiniiriarios/squirrel-table/releases/download/' + releases[0].tag_name + '/SquirrelTableSetup.exe';
